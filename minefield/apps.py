adedf035e97749a60c0597c6f09e5ea463a6147f
Prapti Sharma <64680111+Prapshm@users.noreply.github.com>
2024-07-03 18:49:04 -0500

    Delete minefield directory

--- minefield/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MinefieldConfig(AppConfig):
-    name = 'minefield'
--- minefield/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-def index(request):
-    return render(request,'<h1>MineField</h1>',{})
-# Create your views here.
